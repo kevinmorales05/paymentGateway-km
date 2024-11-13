a20436d001344c88060bf841f78f4b4f36233126
u548742577 <kdmorale@pichincha.com>
2024-11-12 21:08:33 -0700

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Provider } from "@/components/ui/provider"
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,9 +26,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+      <Provider>{children}</Provider>
       </body>
     </html>
   );
---
+++ src/app/login/page.tsx
@@ -0,0 +1,94 @@
+"use client";
+import React from "react";
+//design components
+import {
+  Button,
+  Input,
+  Center,
+  Heading,
+  Container,
+  VStack,
+  Fieldset,
+  Stack,
+  Box,
+  AbsoluteCenter,
+} from "@chakra-ui/react";
+import { Field } from "@/components/ui/field";
+
+import { toaster } from "@/components/ui/toaster";
+//dependency for forms
+import { useForm, SubmitHandler } from "react-hook-form";
+
+// Define form data types
+interface ILoginFormInput {
+  email: string;
+  password: string;
+}
+
+const onSubmit: SubmitHandler<ILoginFormInput> = (data) => {
+  console.log(data);
+  toaster.create({
+    title: "Logged in successfully.",
+    description: "You have successfully logged in.",
+    duration: 5000,
+  });
+};
+
+export default function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ILoginFormInput>();
+
+  // Form submit handler
+  const onSubmit: SubmitHandler<ILoginFormInput> = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <Box>
+      <AbsoluteCenter>
+        <VStack>
+          <Fieldset.Root size="lg" maxW="md">
+            <Heading>Login</Heading>
+            <Stack>
+              <Fieldset.Legend>Access Credentials</Fieldset.Legend>
+              <Fieldset.HelperText>
+                Please provide your email and password
+              </Fieldset.HelperText>
+            </Stack>
+
+            <Fieldset.Content>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                {/* register your input into the hook by invoking the "register" function */}
+
+                <Field label="Email" required>
+                  <Input
+                    defaultValue="zorapay@gmail.com"
+                    {...register("email", { required: true })}
+                  />
+                  {/* errors will return when field validation fails  */}
+                  {errors.email && <span>This field is required</span>}
+                </Field>
+                {/* include validation with required or other standard HTML validation rules */}
+                <Field label="Password" required>
+                  <Input
+                    defaultValue="**********"
+                    {...register("password", { required: true })}
+                  />
+                  {/* errors will return when field validation fails  */}
+                  {errors.password && <span>This field is required</span>}
+                </Field>
+
+                <Button type="submit" alignSelf="flex-start" padding={5} margin={10}>
+                  Submit
+                </Button>
+              </form>
+            </Fieldset.Content>
+          </Fieldset.Root>
+        </VStack>
+      </AbsoluteCenter>
+    </Box>
+  );
+}
